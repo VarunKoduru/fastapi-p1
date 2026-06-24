b4dae50a9d8520e9f430227d7f2634d842121cbf
VarunKoduru <varundeepreddy05@gmail.com>
2026-06-24 23:16:45 +0530

    practiced few http methods

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI
+from models import Product
+
 
 app = FastAPI()
 @app.get("/")
 def welcome():
     return "Hi welcome to fastapi"
+
+products = [Product(id=1,name="phone",price="9999"),Product(id=2,name="laptop",price="55999")]
+
+
+@app.get("/viewp{id}")
+def get_all_products(id:int):
+    for i in products:
+        if i.id == id:
+            return i
+    return "You got scammed"
+
+
+@app.post("/product")
+def add_products(p:Product):
+    products.append(p)
+    return p
+
+
