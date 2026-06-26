52240ec0793fa756014a06ea099cbb76ff014cdd
VarunKoduru <varundeepreddy05@gmail.com>
2026-06-26 12:47:41 +0530

    connected postgresql

---
+++ database_models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, Column, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = "Products"
+
+    id = Column(Integer,primary_key= True,index=True)
+    name = Column(String, index = True)
+    description = Column(String)
+    price = Column(Float)
+    quantity = Column(Integer)
